3ea77099a16ef8d0e250ddf70adf1991ce158535
keshavsoft <nknnkumar@yahoo.com>
2026-07-19 11:02:46 +0530

    3

---
+++ bin/v3/index.js
@@ -0,0 +1,13 @@
+/**
+ * Returns all raw import statements present in the file content.
+ */
+const startFunc = ({ inFileContent, inSearchString }) => {
+    const matches = [];
+    let match;
+    while ((match = inSearchString.exec(inFileContent)) !== null) {
+        matches.push(match[0]);
+    }
+    return matches;
+};
+
+export default startFunc
