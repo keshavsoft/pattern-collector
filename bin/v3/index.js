f9cc34ae89359017318dd11d6735edd24efd6c4d
keshavsoft <nknnkumar@yahoo.com>
2026-07-19 11:19:02 +0530

    act

--- bin/v3/index.js
+++ bin/v3/index.js
@@ -4,8 +4,24 @@
 const startFunc = ({ inFileContent, inSearchString }) => {
     const matches = [];
     let match;
+    const isAppUse = inSearchString.source.includes('app\\.use');
     while ((match = inSearchString.exec(inFileContent)) !== null) {
-        matches.push(match[0]);
+        if (isAppUse) {
+            const startIndex = match.index;
+            const endIndex = startIndex + match[0].length;
+            const start = inFileContent.lastIndexOf('\n', startIndex) + 1;
+            let end = inFileContent.indexOf('\n', endIndex);
+            if (end === -1) {
+                end = inFileContent.length;
+            }
+            let line = inFileContent.substring(start, end);
+            if (line.endsWith('\r')) {
+                line = line.slice(0, -1);
+            }
+            matches.push(line);
+        } else {
+            matches.push(match[0]);
+        }
     }
     return matches;
 };
