9c73cd25f708cc0566bdc61064e44920027cf1af
keshavsoft <nknnkumar@yahoo.com>
2026-07-19 10:46:28 +0530

    use start

--- bin/v2/index.js
+++ bin/v2/index.js
@@ -2,7 +2,7 @@
  * Returns all raw import statements present in the file content.
  */
 const startFunc = ({ inFileContent, inSearchString }) => {
-    const importRegex = /import\s+[\s\S]*?\s+from\s+['"][^'"]+['"]|import\s+['"][^'"]+['"]/g;
+    // const importRegex = /import\s+[\s\S]*?\s+from\s+['"][^'"]+['"]|import\s+['"][^'"]+['"]/g;
     const matches = [];
     let match;
     while ((match = inSearchString.exec(inFileContent)) !== null) {
